e96262590603e0f7ce81d3657304451f00440555
Yanik2 <wm_yanik@ukr.net>
2025-03-01 16:40:21 +0100

    convert record to class

--- src/main/java/com/imageprocessing/PubSubMessage.java
+++ src/main/java/com/imageprocessing/PubSubMessage.java
@@ -1,9 +1,51 @@
 package com.imageprocessing;
 
+import java.util.Objects;
 import java.util.UUID;
 
-public record PubSubMessage(
-    String imageName,
-    UUID processId
-) {
+public final class PubSubMessage {
+    private final String imageName;
+    private final UUID processId;
+
+    public PubSubMessage(
+        String imageName,
+        UUID processId
+    ) {
+        this.imageName = imageName;
+        this.processId = processId;
+    }
+
+    public String imageName() {
+        return imageName;
+    }
+
+    public UUID processId() {
+        return processId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (PubSubMessage) obj;
+        return Objects.equals(this.imageName, that.imageName) &&
+            Objects.equals(this.processId, that.processId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(imageName, processId);
+    }
+
+    @Override
+    public String toString() {
+        return "PubSubMessage[" +
+            "imageName=" + imageName + ", " +
+            "processId=" + processId + ']';
+    }
+
 }
